79f48a96b03577248a226abb90cdc84337cc42e0
Kristina Smirnova <kristina_smirnova@epam.com>
2019-11-11 12:09:09 +0300

    Fix for temporal test directories (for windows tests)

--- test-framework/src/main/kotlin/com/epam/drill/e2e/AdminTest.kt
+++ test-framework/src/main/kotlin/com/epam/drill/e2e/AdminTest.kt
@@ -2,13 +2,13 @@ package com.epam.drill.e2e
 
 import com.epam.kodux.*
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.io.*
 import java.io.*
+import java.util.*
 
 abstract class AdminTest {
 
-    @TempDir
-    lateinit var projectDir: File
+    val projectDir = File(System.getProperty("java.io.tmpdir") + File.separator + UUID.randomUUID())
+
     lateinit var globToken: String
     lateinit var storeManager: StoreManager
 
