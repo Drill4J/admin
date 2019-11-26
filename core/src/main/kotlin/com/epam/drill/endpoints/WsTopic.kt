a5b90eb63a712ec834162053eb94607ee57c10f5
Viktoriia Lebedeva <Viktoriia_Lebedeva@epam.com>
2019-11-26 18:59:21 +0300

    Refactoring find map by URl and test writing

--- core/src/main/kotlin/com/epam/drill/endpoints/WsTopic.kt
+++ core/src/main/kotlin/com/epam/drill/endpoints/WsTopic.kt
@@ -8,15 +8,17 @@ import kotlinx.serialization.*
 import kotlinx.serialization.internal.*
 import org.kodein.di.*
 import org.kodein.di.generic.*
+import java.lang.RuntimeException
 import java.util.concurrent.*
 import kotlin.collections.set
 import kotlin.reflect.*
 import kotlin.reflect.full.*
 
+private val regexPathParam = "\\{(.*)}".toRegex()
+
 class WsTopic(override val kodein: Kodein) : KodeinAware {
     private val app: Application by instance()
-    val pathToCallBackMapping: MutableMap<String, Pair<KClass<*>, CallbackWrapper<Any, Any>>> = ConcurrentHashMap()
-    private val p = "\\{(.*)}".toRegex()
+    val pathToCallBackMapping: MutableMap<URLTopic, Pair<KClass<*>, CallbackWrapper<Any, Any>>> = ConcurrentHashMap()
 
     suspend operator fun invoke(block: suspend WsTopic.() -> Unit) {
         block(this)
@@ -30,43 +32,56 @@ class WsTopic(override val kodein: Kodein) : KodeinAware {
     }
 
     fun getParams(destination: String): Pair<CallbackWrapper<Any, Any>, Any> {
-        val urlTokens = destination.split("/")
-
-        val filter = pathToCallBackMapping.filter { it.key.count { c -> c == '/' } + 1 == urlTokens.size }.filter {
-            var matche = true
-            it.key.split("/").forEachIndexed { x, y ->
-                if (y == urlTokens[x] || y.startsWith("{")) {
-                } else {
-                    matche = false
-                }
-            }
-            matche
-        }
-        val suitableRout = filter.entries.first()
-
-        val parameters = suitableRout.run {
-            val mutableMapOf = mutableMapOf<String, String>()
-            key.split("/").forEachIndexed { x, y ->
-                if (y == urlTokens[x]) {
-                } else if (p.matches(y)) {
-                    mutableMapOf[p.find(y)!!.groupValues[1]] = urlTokens[x]
-                }
-            }
-            val map = mutableMapOf.map { Pair(it.key, listOf(it.value)) }
-            parametersOf(* map.toTypedArray())
-        }
+        val (suitableRout, parameters) = pathToCallBackMapping.suitableRoutWithParameters(destination)
 
         val (routeClass, callback) = suitableRout.value
         return callback to app.feature(Locations).resolve(routeClass, parameters)
-
     }
 }
+
+
+typealias Routs = Map<URLTopic, Pair<KClass<*>, CallbackWrapper<Any, Any>>>
+
+fun Routs.suitableRoutWithParameters(destination: String) = run {
+    val urlTokens = destination.split("/")
+    val filteredRouts = this.filterRoutsByLength(urlTokens).filterRoutsByUrl(urlTokens)
+    if (filteredRouts.isEmpty()) throw RuntimeException("A destination '$destination' is not registered")
+    val suitableRout = filteredRouts.routWithMinParametersCount()
+    val mutableMapOfParam = suitableRout.key.getMapOfParameters(urlTokens)
+    val pairsOfParam = mutableMapOfParam.map { (paramName, paramValue) -> paramName to listOf(paramValue) }
+    suitableRout to parametersOf(* pairsOfParam.toTypedArray())
+}
+
+fun Routs.routWithMinParametersCount() = this.entries.first { it.key == this.keys.min() }
+
+fun Routs.filterRoutsByUrl(urlTokens: List<String>) = this.filter { it.key.isMatched(urlTokens) }
+
+fun Routs.filterRoutsByLength(urlTokens: List<String>) = this.filter { it.key.tokens().size == urlTokens.size }
+
+data class URLTopic(val rawUrl: String) : Comparable<URLTopic> {
+
+    fun tokens() = rawUrl.split("/")
+
+    override fun compareTo(other: URLTopic) = this.rawUrl.count { it == '{' } - other.rawUrl.count { it == '{' }
+
+    fun getMapOfParameters(urlTokens: List<String>) = tokens()
+        .mapIndexed { index, pathParam -> index to pathParam }
+        .filter { (index, pathParam) -> pathParam != urlTokens[index] && regexPathParam.matches(pathParam) }
+        .map { (index, pathParam) ->
+            val (pathParamTemplate) = regexPathParam.find(pathParam)!!.destructured
+            pathParamTemplate to urlTokens[index]
+        }
+
+    fun isMatched(urlTokens: List<String>) =
+        this.tokens().filterIndexed { index, token -> token != urlTokens[index] && !token.startsWith("{") }.isEmpty()
+}
+
 
 inline fun <reified R : Any> WsTopic.topic(noinline block: (R) -> Any?) {
     val findAnnotation = R::class.findAnnotation<Location>()
     val path = findAnnotation?.path!!
     @Suppress("UNCHECKED_CAST")
-    pathToCallBackMapping[path] = R::class to CallbackWrapper(block) as CallbackWrapper<Any, Any>
+    pathToCallBackMapping[URLTopic(path)] = R::class to CallbackWrapper(block) as CallbackWrapper<Any, Any>
 }
 
 class CallbackWrapper<T, R>(val block: (R) -> T?) {
--- core/src/test/kotlin/com/epam/drill/websockets/WsTopicTest.kt
+++ core/src/test/kotlin/com/epam/drill/websockets/WsTopicTest.kt
@@ -2,10 +2,18 @@ package com.epam.drill.websockets
 
 import com.epam.drill.common.*
 import com.epam.drill.endpoints.*
-import kotlinx.serialization.*
-import kotlinx.serialization.json.*
+import io.ktor.http.*
 import org.junit.Test
+import kotlin.reflect.*
 import kotlin.test.*
+
+typealias Pair = kotlin.Pair<Map.Entry<URLTopic, kotlin.Pair<KClass<*>, CallbackWrapper<Any, Any>>>, Parameters>
+
+val Pair.url
+    get() = first.key
+
+val Pair.parameters
+    get() = second
 
 internal class WsTopicKtTest {
     @Test
@@ -25,5 +33,63 @@ internal class WsTopicKtTest {
         assertEquals(result2, serializedStructure2)
         assertEquals("[]", serialize(emptyArray<String>()))
     }
+
+    private val testMutableMap = mutableMapOf(
+        URLTopic("/{any}/{anywhere}/{anyhow}") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/{any}/{anywhere}/xxx") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/{any}/xxx/{anywhere}") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/{any}/xxx/yyy") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/xxx/{any}/{anywhere}") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/xxx/{any}/yyy") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper {}),
+        URLTopic("/xxx/yyy/{any}") to (WsTopicKtTest::class as KClass<*> to CallbackWrapper<Any, Any> {})
+    )
+
+    private fun par(st: String) = testMutableMap.suitableRoutWithParameters(st)
+
+
+    @Test
+    fun `check method find Map And Parameters By Url`() {
+        with(par("/xxx/random2/yyy")) {
+            assertEquals(URLTopic("/xxx/{any}/yyy"), this.url)
+            assertEquals(this.parameters["any"], "random2")
+        }
+
+        with(par("/random1/xxx/yyy")) {
+            assertEquals(URLTopic("/{any}/xxx/yyy"), this.url)
+            assertEquals("random1", this.parameters["any"])
+        }
+
+        with(par("/xxx/yyy/random3")) {
+            assertEquals(URLTopic("/xxx/yyy/{any}"), this.url)
+            assertEquals("random3", this.parameters["any"])
+        }
+
+        with(par("/xxx/random2/random3")) {
+            assertEquals(URLTopic("/xxx/{any}/{anywhere}"), this.url)
+            assertEquals("random2", this.parameters["any"])
+            assertEquals("random3", this.parameters["anywhere"])
+        }
+
+        with(par("/random1/xxx/random3")) {
+            assertEquals(URLTopic("/{any}/xxx/{anywhere}"), this.url)
+            assertEquals("random1", this.parameters["any"])
+            assertEquals("random3", this.parameters["anywhere"])
+        }
+
+        with(par("/random1/random2/xxx")) {
+            assertEquals(URLTopic("/{any}/{anywhere}/xxx"), this.url)
+            assertEquals("random2", this.parameters["anywhere"])
+            assertEquals("random1", this.parameters["any"])
+        }
+
+        with(par("/random1/random2/random3")) {
+            assertEquals(URLTopic("/{any}/{anywhere}/{anyhow}"), this.url)
+            assertEquals("random1", this.parameters["any"])
+            assertEquals("random2", this.parameters["anywhere"])
+            assertEquals("random3", this.parameters["anyhow"])
+        }
+
+    }
+
 
 }
