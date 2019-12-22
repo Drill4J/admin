ad4aa378503ac740b9371d5b27960b1c24e9bdc4
Viktor Bellavin <viktor_bellavin@epam.com>
2019-12-23 00:56:25 +0300

    EPMDJ-2066 Separate admin repo - github actions; caching for check workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,10 @@ include(":test-framework:test-data")
 include(":test-framework:test-plugin")
 include(":test-framework:test-plugin:admin-part")
 include(":test-framework:test-plugin:agent-part")
+
+buildCache {
+    local {
+        directory = rootDir.resolve("build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+}
