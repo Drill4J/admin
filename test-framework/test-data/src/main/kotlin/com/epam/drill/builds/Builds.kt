d48a3ee1dd0d06101b08a4b0b54cacfa5b85da1e
IgorKey <Igor_Kuzminykh@epam.com>
2019-12-27 13:23:28 +0300

    fixed links to build entities (test-framework)

--- test-framework/test-data/src/main/kotlin/com/epam/drill/builds/Builds.kt
+++ test-framework/test-data/src/main/kotlin/com/epam/drill/builds/Builds.kt
@@ -2,7 +2,7 @@ package com.epam.drill.builds
 
 object Build1 : Build {
     override lateinit var test: Class<Tst>
-    fun entryPoint() = test.constructors.first().newInstance() as Test
+    fun entryPoint() = Build1.test.constructors.first().newInstance() as Test
     override val name: String = "build1"
 
 
@@ -17,7 +17,7 @@ object Build1 : Build {
 object Build2 : Build {
     override lateinit var test: Class<Tst>
     override val name: String = "build2"
-    fun entryPoint() = Build1.test.constructors.first().newInstance() as Test
+    fun entryPoint() = Build2.test.constructors.first().newInstance() as Test
 
     interface Test : Tst {
         fun test1()
@@ -29,7 +29,7 @@ object Build2 : Build {
 object Build3 : Build {
     override lateinit var test: Class<Tst>
     override val name: String = "build3"
-    fun entryPoint() = Build1.test.constructors.first().newInstance() as Test
+    fun entryPoint() = Build3.test.constructors.first().newInstance() as Test
 
     interface Test : Tst {
         fun test1()
@@ -41,7 +41,7 @@ object Build3 : Build {
 object Build4 : Build {
     override lateinit var test: Class<Tst>
     override val name: String = "build4"
-    fun entryPoint() = Build1.test.constructors.first().newInstance() as Test
+    fun entryPoint() = Build4.test.constructors.first().newInstance() as Test
 
     interface Test : Tst {
         fun test1()
@@ -53,7 +53,7 @@ object Build4 : Build {
 object Build5 : Build {
     override lateinit var test: Class<Tst>
     override val name: String = "build5"
-    fun entryPoint() = Build1.test.constructors.first().newInstance() as Test
+    fun entryPoint() = Build5.test.constructors.first().newInstance() as Test
     interface Test : Tst {
         fun test1()
         fun test2()
