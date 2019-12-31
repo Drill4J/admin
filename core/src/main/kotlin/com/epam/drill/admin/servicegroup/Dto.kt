3d3b64226a0b12bcca9279045d5b25df20db7f1c
Viktor Bellavin <viktor_bellavin@epam.com>
2019-12-31 21:31:47 +0300

    EPMDJ-2013 Plugin summaries of a service group.

--- core/src/main/kotlin/com/epam/drill/admin/servicegroup/Dto.kt
+++ core/src/main/kotlin/com/epam/drill/admin/servicegroup/Dto.kt
@@ -1,5 +1,6 @@
 package com.epam.drill.admin.servicegroup
 
+import com.epam.drill.admindata.*
 import com.epam.drill.agentmanager.*
 import com.epam.drill.common.*
 import com.epam.drill.endpoints.*
@@ -17,6 +18,20 @@ data class GroupedAgentsDto(
     val grouped: List<ServiceGroupDto>
 )
 
+@Serializable
+data class PluginSummaryDto(
+    val agentName: String,
+    val lastBuild: String,
+    @ContextualSerialization val data: Any
+)
+
+@Serializable
+data class ServiceGroupSummaryDto(
+    val name: String,
+    val summaries: List<PluginSummaryDto>,
+    @ContextualSerialization val aggregatedData: Any
+)
+
 fun GroupedAgents.toDto(agentManager: AgentManager) = GroupedAgentsDto(
     single = first.agentInfos.toDto(agentManager),
     grouped = second.map { agentGroup ->
@@ -25,6 +40,12 @@ fun GroupedAgents.toDto(agentManager: AgentManager) = GroupedAgentsDto(
             agents = agentGroup.agentInfos.toDto(agentManager)
         )
     }
+)
+
+internal fun AgentEntry.toPluginSummaryDto(adminData: AdminPluginData, data: Any) = PluginSummaryDto(
+    agentName = agent.name,
+    lastBuild = adminData.buildManager.lastBuild,
+    data = data
 )
 
 private fun List<AgentInfo>.toDto(agentManager: AgentManager) = map { it.toDto(agentManager) }
