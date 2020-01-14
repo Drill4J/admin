75a515bbe68f39eeb3e660744e7f795d1b7d0be3
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-01-14 16:34:34 +0300

    EPMDJ-2173. Changing the 'description' field to optional.

--- core/src/test-integration/kotlin/com/epam/drill/e2e/AgentRegistrationTest.kt
+++ core/src/test-integration/kotlin/com/epam/drill/e2e/AgentRegistrationTest.kt
@@ -1,6 +1,7 @@
 package com.epam.drill.e2e
 
 import com.epam.drill.common.*
+import com.epam.drill.endpoints.agent.*
 import io.kotlintest.*
 import io.ktor.http.*
 import kotlin.test.*
@@ -16,6 +17,29 @@ class AgentRegistrationTest : E2ETest() {
             connectAgent(AgentWrap(agentId, "0.1.0")) { ui, agent ->
                 ui.getAgent()?.status shouldBe AgentStatus.NOT_REGISTERED
                 register(agentId) { status, _ ->
+                    status shouldBe HttpStatusCode.OK
+                }
+                ui.getAgent()?.status shouldBe AgentStatus.BUSY
+                agent.`get-set-packages-prefixes`()
+                agent.`get-load-classes-datas`()
+                ui.getAgent()?.status shouldBe AgentStatus.ONLINE
+            }
+        }
+    }
+
+    @Test
+    fun `agent must register without a description`() {
+        createSimpleAppWithUIConnection {
+            connectAgent(AgentWrap(agentId, "0.1.0")) { ui, agent ->
+                ui.getAgent()?.status shouldBe AgentStatus.NOT_REGISTERED
+                register(
+                    agentId,
+                    payload = AgentRegistrationInfo(
+                        name = "without description",
+                        packagesPrefixes = listOf("testPrefix"),
+                        plugins = emptyList()
+                    )
+                ) { status, _ ->
                     status shouldBe HttpStatusCode.OK
                 }
                 ui.getAgent()?.status shouldBe AgentStatus.BUSY
