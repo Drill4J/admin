d83c2546d247b488ff5949bd350fe93ec364146d
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-07-24 13:35:06 +0300

    EPMDJ-2944 Removed unused fields from BuildSummaryDto

--- core/src/main/kotlin/build/Dto.kt
+++ core/src/main/kotlin/build/Dto.kt
@@ -6,10 +6,5 @@ import kotlinx.serialization.*
 data class BuildSummaryDto(
     val buildVersion: String = "",
     val detectedAt: Long = 0L,
-    val totalMethods: Int = 0,
-    val newMethods: Int = 0,
-    val modifiedMethods: Int = 0,
-    val unaffectedMethods: Int = 0,
-    val deletedMethods: Int = 0,
     @ContextualSerialization val summary: Any? = null
 )
