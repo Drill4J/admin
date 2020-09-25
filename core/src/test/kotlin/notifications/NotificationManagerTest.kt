a741cd78ff0e43852255ea56b30b4d347e41a969
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-09-25 13:45:13 +0300

    EPMDJ-4347 Added tests for bulk operations with notifications

--- core/src/test/kotlin/notifications/NotificationManagerTest.kt
+++ core/src/test/kotlin/notifications/NotificationManagerTest.kt
@@ -43,4 +43,37 @@ class NotificationManagerTest {
         assertNotNull(notificationManager.notifications[id])
         assertTrue { notificationManager.notifications[id]!!.read }
     }
+
+    @Test
+    fun `readAll - statuses must be changed`() {
+        val id = notification.id
+        val notification = notification.copy(id = "second-notification")
+        notificationManager.save(notification)
+        assertEquals(2, notificationManager.readAll().valuesDesc.size)
+        assertTrue { notificationManager.notifications[id]!!.read }
+        assertTrue { notificationManager.notifications[notification.id]!!.read }
+    }
+
+    @Test
+    fun `delete - not existing notification`() {
+        val id = "not-existing"
+        assertFalse { notificationManager.delete(id) }
+        assertNull(notificationManager.notifications[id])
+    }
+
+    @Test
+    fun `delete - notification must be deleted`() {
+        val id = notification.id
+        assertNotNull(notificationManager.notifications[id])
+        assertTrue { notificationManager.delete(id) }
+        assertNull(notificationManager.notifications[id])
+    }
+
+    @Test
+    fun `deleteAll - notifications must be deleted`() {
+        assertEquals(1, notificationManager.notifications.valuesDesc.size)
+        val notifications = notificationManager.deleteAll()
+        assertEquals(1, notifications.valuesDesc.size)
+        assertEquals(0, notificationManager.notifications.valuesDesc.size)
+    }
 }
