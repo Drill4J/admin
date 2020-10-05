863bb8b0759dc65e5e6fa025e6bade505ad70686
IgorKey <Igor_Kuzminykh@epam.com>
2020-10-05 19:58:08 +0300

    EPMDJ-4802 minor fix

--- core/src/main/kotlin/endpoints/agent/AgentHandler.kt
+++ core/src/main/kotlin/endpoints/agent/AgentHandler.kt
@@ -72,7 +72,7 @@ class AgentHandler(override val kodein: Kodein) : KodeinAware {
                         withContext(Dispatchers.IO) {
                             val rawContent = frame.readBytes()
                             val frameBytes = if (useCompression) {
-                                Zstd.decode(rawContent)
+                                Zstd.decompress(rawContent)
                             } else rawContent
                             BinaryMessage(ProtoBuf.load(Message.serializer(), frameBytes))
                         }
