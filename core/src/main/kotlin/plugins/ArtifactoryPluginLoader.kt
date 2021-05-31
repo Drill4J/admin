f9eaa3e3195132ed69613921d6298ba04d0c35c1
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2021-05-31 21:10:17 +0300

    EPMDJ-7398 Added a parameter specifying where to load the plugin from, by default is github

--- core/src/main/kotlin/plugins/ArtifactoryPluginLoader.kt
+++ core/src/main/kotlin/plugins/ArtifactoryPluginLoader.kt
@@ -18,20 +18,19 @@ package com.epam.drill.admin.plugins
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import kotlinx.serialization.json.*
 import mu.*
 import java.io.*
 
 private val logger = KotlinLogging.logger {}
 
 data class ArtifactoryPluginLoader(
-    val baseUrl: String,
-    val basePath: String,
-    val repo: String,
+    val artifactory: Artifactory,
     val storageDir: File,
     val devMode: Boolean = true //TODO dev mode handling
 ) {
     suspend fun loadPlugins(pluginIds: Iterable<String>) {
-        logger.info { "Loading plugins $pluginIds from artifactory '$baseUrl'..." }
+        logger.info { "Loading plugins $pluginIds from artifactory '${artifactory.baseUrl}'..." }
         HttpClient(CIO).use { client ->
             for (pluginId in pluginIds) {
                 client.load(pluginId)
@@ -44,47 +43,75 @@ data class ArtifactoryPluginLoader(
         val version: String = getVersion(pluginId)
         logger.info { "Loading $pluginId v$version" }
         val targetFilename = "$pluginId-plugin-$version.zip"
-        val artifactPath = "com/epam/drill/plugins/$pluginId-plugin/$version/$targetFilename"
         val targetFile = storageDir.resolve(targetFilename)
         if (devMode) {
+            val artifactPath = "com/epam/drill/plugins/$pluginId-plugin/$version/$targetFilename"
             val localMavenRepo = File(System.getProperty("user.home"), ".m2").resolve("repository")
             val artifactFile = localMavenRepo.resolve(artifactPath)
             if (artifactFile.exists()) {
                 logger.info { "Copying $artifactFile from local maven repo" }
                 artifactFile.copyTo(targetFile, overwrite = true)
             } else {
-                downloadPlugin(artifactPath, targetFile)
+                downloadPlugin(pluginId, version, targetFile)
             }
         } else {
-            downloadPlugin(artifactPath, targetFile)
+            downloadPlugin(pluginId, version, targetFile)
         }
     }
 
-    private suspend fun HttpClient.getVersion(pluginId: String): String = run {
-        when (val envVersion = pluginId.toEnvVersion()) {
-            "", "latest" -> get(baseUrl) {
-                url.path(basePath, "api/search/latestVersion")
-                url.parameters.apply {
-                    append("g", PLUGIN_PACKAGE)
-                    append("a", "$pluginId-plugin")
-                    append("v", " ")
-                    append("repos", repo)
+    private suspend fun HttpClient.getVersion(pluginId: String): String = pluginId.toEnvVersion().let { envVersion ->
+        if (envVersion in listOf("", "latest")) {
+            when (artifactory) {
+                Artifactory.GITHUB -> run {
+                    val releases = get<String>(artifactory.baseUrl) {
+                        url.path(artifactory.basePath, artifactory.repo, "$pluginId-plugin", "releases")
+                        url.parameters.append("prerelease", "true")
+                    }.let { Json.parseToJsonElement(it) }
+                    releases.jsonArray.first().jsonObject["tag_name"]?.jsonPrimitive?.content?.replace("v", "") ?: ""
+                }
+                else -> run {
+                    get(artifactory.baseUrl) {
+                        url.path(artifactory.basePath, "api/search/latestVersion")
+                        url.parameters.apply {
+                            append("g", PLUGIN_PACKAGE)
+                            append("a", "$pluginId-plugin")
+                            append("v", " ")
+                            append("repos", artifactory.repo)
+                        }
+                    }
                 }
             }
-            else -> envVersion
-        }
+        } else envVersion
     }
 
-    private fun String.toEnvVersion(): String  = run {
+    private fun String.toEnvVersion(): String = run {
         val normalizedId = replace(Regex("\\s|-"), "_").toUpperCase()
         System.getenv("${normalizedId}_PLUGIN_VERSION") ?: ""
     }
 
-    private suspend fun HttpClient.downloadPlugin(artifactPath: String, targetFile: File) {
-        logger.info { "Downloading $artifactPath" }
-        val pluginBytes: ByteArray = get(baseUrl) {
-            url.path(basePath, repo, artifactPath)
+    private suspend fun HttpClient.downloadPlugin(pluginId: String, version: String, targetFile: File) {
+        val pluginBytes: ByteArray = when (artifactory) {
+            Artifactory.GITHUB -> run {
+                val artifactPath = "$pluginId-plugin/releases/download/v$version/${targetFile.name}"
+                logger.info { "Downloading $artifactPath" }
+                get(artifactory.baseUrl.replace("api.", "")) {
+                    url.path(artifactory.repo, artifactPath)
+                }
+            }
+            else -> run {
+                val artifactPath = "com/epam/drill/plugins/$pluginId-plugin/$version/${targetFile.name}"
+                logger.info { "Downloading $artifactPath" }
+                get(artifactory.baseUrl) {
+                    url.path(artifactory.basePath, artifactory.repo, artifactPath)
+                }
+            }
         }
         targetFile.writeBytes(pluginBytes)
     }
+}
+
+enum class Artifactory(val baseUrl: String, val basePath: String, val repo: String) {
+    GITHUB("https://api.github.com/", "repos", "Drill4J"),
+    OSS_JFROG("https://oss.jfrog.org", "artifactory", "oss-release-local"),
+    DRILL4J_JFROG("https://drill4j.jfrog.io", "artifactory", "drill")
 }
