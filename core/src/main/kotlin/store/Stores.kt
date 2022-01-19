ae07e678fff82116da03c950107d88b6e78461f5
LikhanovMaxim <LikhanovMaxim@yandex.ru>
2022-01-19 15:00:30 +0300

    EPMDJ-9704 [BE] Error when finishing non-empty active scopes in the Service Group

--- core/src/main/kotlin/store/Stores.kt
+++ core/src/main/kotlin/store/Stores.kt
@@ -16,9 +16,33 @@
 package com.epam.drill.admin.store
 
 import com.epam.dsm.*
+import kotlinx.coroutines.sync.*
+import mu.*
+import java.util.*
 
-fun pluginStoresDSM(pluginId: String): StoreClient =
-    StoreClient("plugins_${pluginId.toLowerCase().replace('-', '_')}")
+private val logger = KotlinLogging.logger {}
+private val storeClientPlugins = mutableMapOf<String, StoreClient>()
+
+private val mutex = Mutex()
+suspend fun pluginStoresDSM(pluginId: String): StoreClient =
+    storeClientPlugins[pluginId] ?: createOncePluginStore(pluginId)
+
+private suspend fun createOncePluginStore(pluginId: String): StoreClient {
+    mutex.withLock {
+        val storeClient = storeClientPlugins[pluginId]
+        if (storeClient != null) {
+            logger.trace { "get store client $pluginId" }
+            return storeClient
+        }
+        logger.info { "create a new store client $pluginId" }
+        val newStoreClient = StoreClient(pluginSchemaName(pluginId))
+        storeClientPlugins[pluginId] = newStoreClient
+        return newStoreClient
+    }
+}
+
+fun pluginSchemaName(pluginId: String) =
+    "plugins_${pluginId.lowercase(Locale.getDefault()).replace('-', '_')}"
 
 val commonStore = StoreClient("common")
 val agentStores = StoreClient("agents")
