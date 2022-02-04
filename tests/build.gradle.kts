a4b601b7e28d43812d10408c70365e2e06bc25a0
LikhanovMaxim <maksim_likhanov@epam.com>
2022-02-04 16:56:14 +0300

    EPMDJ-8975 Ability of a User to filter Test2Code plugin data by attributes update drill Api and DSM

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -60,8 +60,6 @@ dependencies {
     testImplementation("io.ktor:ktor-client-cio:$ktorVersion")
     testImplementation("io.ktor:ktor-serialization:$ktorVersion")
 
-    testImplementation("com.epam.drill.dsm:core:$drillDsmVersion")
-    testImplementation("com.epam.drill.dsm:test-framework:$drillDsmVersion")
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
     //TODO remove logging - EPMDJ-9548
     testImplementation("io.github.microutils:kotlin-logging-jvm:$muLogger")
