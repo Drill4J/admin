2fc7f79c9a635b572fcf4db4d3c4f02aaae24a92
igorkuzminykh <kuzminykh.igor@gmail.com>
2022-05-24 15:38:54 +0300

    EPMDJ-9980 Added sources for test-data

--- test-framework/test-data/build.gradle.kts
+++ test-framework/test-data/build.gradle.kts
@@ -18,13 +18,18 @@ tasks {
 
 java {
     targetCompatibility = JavaVersion.VERSION_1_8
-    withSourcesJar()
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
 }
 
 publishing {
     publications {
         create<MavenPublication>(project.name) {
             artifact(tasks["jar"])
+            artifact(sourcesJar.get())
         }
     }
 }
