1001596df019eb304c41bb0955db94a1e2f402b4
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2023-10-13 21:35:05 +0300

    feat: rework session updating

--- test2code-admin/src/main/kotlin/com/epam/drill/plugins/test2code/State.kt
+++ test2code-admin/src/main/kotlin/com/epam/drill/plugins/test2code/State.kt
@@ -238,16 +238,11 @@ internal class AgentState(
                 "FinishSession. size of exec data = ${testSession.probes.values.map { it.keys }.count()}"
             }.also { logPoolStats() }
             trackTime("session storing") {
-                if (testSession.isFinished){
-                    storeClient.updateSession(testSession)
-                } else {
-                    testSession.isFinished = true
-                    storeClient.storeSession(
-                        sessionHolder.id,
-                        agentKey,
-                        testSession
-                    )
-                }
+                storeClient.storeSession(
+                    sessionHolder.id,
+                    agentKey,
+                    testSession
+                )
             }
             logger.debug { "Session $sessionId finished." }.also { logPoolStats() }
         } else logger.debug { "Session with id $sessionId is empty, it won't be added to the sessionHolder." }
