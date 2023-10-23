61da90fce370e69948c70208f4cf7881ce4eab57
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2023-10-23 19:57:48 +0300

    refactor: change scope to sessionHolder at test

--- test2code-admin/src/test/kotlin/com/epam/drill/plugins/test2code/JsCoverageTest.kt
+++ test2code-admin/src/test/kotlin/com/epam/drill/plugins/test2code/JsCoverageTest.kt
@@ -30,7 +30,7 @@ class JsCoverageTest : PostgresBased("js_coverage") {
     private val manualTestType = "MANUAL"
 
     @Test
-    fun `coverageData for scope with custom js probes`() {
+    fun `coverageData for sessionHolder with custom js probes`() {
         runBlocking {
             val coverageData = calculateCoverage() {
                 this.execSession(manualTestType) { sessionId ->
