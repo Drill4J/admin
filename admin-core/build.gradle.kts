f39c90dc6ed92187e00c4a3d752417d9b535911d
bodyangug <aseevbogdan5@gmail.com>
2023-11-16 20:32:51 +0200

    fix: rework env vars

--- admin-core/build.gradle.kts
+++ admin-core/build.gradle.kts
@@ -127,8 +127,8 @@ val apiPort = "8090"
 val debugPort = "5006"
 val secureApiPort = "8453"
 val jibExtraDirs = "$buildDir/jib-extra-dirs"
-val gitUsername = ""
-val gitPassword = ""
+val gitUsername = System.getenv("GH_USERNAME") ?: ""
+val gitPassword = System.getenv("GH_TOKEN") ?: ""
 jib {
     from {
         image = "adoptopenjdk/openjdk11:latest"
