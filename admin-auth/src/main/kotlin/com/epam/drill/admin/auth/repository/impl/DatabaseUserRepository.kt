9e2d50b1cbabbf9f75c333c8a2c74f003f223512
iryabov <to.iryabov@gmail.com>
2023-11-29 13:56:41 +0100

    refactor: move getting the current date time to a separate function
    EPMDJ-10712

--- admin-auth/src/main/kotlin/com/epam/drill/admin/auth/repository/impl/DatabaseUserRepository.kt
+++ admin-auth/src/main/kotlin/com/epam/drill/admin/auth/repository/impl/DatabaseUserRepository.kt
@@ -22,7 +22,9 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import java.time.LocalDateTime
 
-class DatabaseUserRepository : UserRepository {
+class DatabaseUserRepository(
+    val currentDateTimeProvider: () -> LocalDateTime = { LocalDateTime.now() }
+) : UserRepository {
     override suspend fun findAll(): List<UserEntity> {
         return UserTable.selectAll().map { it.toEntity() }
     }
@@ -32,7 +34,8 @@ class DatabaseUserRepository : UserRepository {
     }
 
     override suspend fun findByUsername(username: String): UserEntity? {
-        return UserTable.select { UserTable.username.lowerCase() eq username.lowercase() }.map { it.toEntity() }.firstOrNull()
+        return UserTable.select { UserTable.username.lowerCase() eq username.lowercase() }.map { it.toEntity() }
+            .firstOrNull()
     }
 
     override suspend fun create(entity: UserEntity): UserEntity {
@@ -50,21 +53,21 @@ class DatabaseUserRepository : UserRepository {
     override suspend fun deleteById(id: Int) {
         UserTable.deleteWhere { UserTable.id eq id }
     }
-}
 
-private fun ResultRow.toEntity() = UserEntity(
-    id = this[UserTable.id].value,
-    username = this[UserTable.username],
-    passwordHash = this[UserTable.passwordHash],
-    role = this[UserTable.role],
-    blocked = this[UserTable.blocked],
-    registrationDate = this[UserTable.registrationDate]
-)
+    private fun ResultRow.toEntity() = UserEntity(
+        id = this[UserTable.id].value,
+        username = this[UserTable.username],
+        passwordHash = this[UserTable.passwordHash],
+        role = this[UserTable.role],
+        blocked = this[UserTable.blocked],
+        registrationDate = this[UserTable.registrationDate]
+    )
 
-private fun UserEntity.mapTo(builder: UpdateBuilder<Int>) {
-    builder[UserTable.username] = username
-    builder[UserTable.passwordHash] = passwordHash
-    builder[UserTable.role] = role
-    builder[UserTable.blocked] = blocked
-    builder[UserTable.registrationDate] = registrationDate ?: LocalDateTime.now()
+    private fun UserEntity.mapTo(builder: UpdateBuilder<Int>) {
+        builder[UserTable.username] = username
+        builder[UserTable.passwordHash] = passwordHash
+        builder[UserTable.role] = role
+        builder[UserTable.blocked] = blocked
+        builder[UserTable.registrationDate] = registrationDate ?: currentDateTimeProvider()
+    }
 }
