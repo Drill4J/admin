6be66355fb2f05e75b2ed0a93660a7aa7249321b
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-02-19 19:50:36 +0400

    chore: add license headers

--- test2code-admin/src/main/kotlin/com/epam/drill/plugins/test2code/multibranch/repository/RawDataRepositoryImpl.kt
+++ test2code-admin/src/main/kotlin/com/epam/drill/plugins/test2code/multibranch/repository/RawDataRepositoryImpl.kt
@@ -1,14 +1,29 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.drill.plugins.test2code.multibranch.repository
 
 import com.epam.drill.common.agent.configuration.AgentMetadata
 import com.epam.drill.common.agent.configuration.AgentType
 import com.epam.drill.plugins.test2code.api.AddTestsPayload
-import com.epam.drill.plugins.test2code.common.api.*
+import com.epam.drill.plugins.test2code.common.api.Probes
 import com.epam.drill.plugins.test2code.common.transport.ClassMetadata
 import com.epam.drill.plugins.test2code.common.transport.CoverageData
+import com.epam.drill.plugins.test2code.multibranch.rawdata.config.DatabaseConfig
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
-import com.epam.drill.plugins.test2code.multibranch.rawdata.config.DatabaseConfig
 
 private const val LONG_TEXT_LENGTH = 65535 // java class name max len
 private const val MEDIUM_TEXT_LENGTH = 2000
