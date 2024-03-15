63dc1d18edd332b5c5c6ad47cc97fbc84ae51db0
iryabov <to.iryabov@gmail.com>
2024-03-15 09:57:52 +0100

    refactor: make a route hierarchy
    EPMDJ-10811

--- admin-writer/src/main/kotlin/com/epam/drill/admin/writer/rawdata/route/AgentConfigRoutes.kt
+++ admin-writer/src/main/kotlin/com/epam/drill/admin/writer/rawdata/route/AgentConfigRoutes.kt
@@ -29,28 +29,31 @@ import java.util.zip.GZIPInputStream
 import kotlinx.serialization.protobuf.ProtoBuf
 import kotlinx.serialization.json.Json
 
-object Paths {
-    @Location("/api/groups/{groupId}/agents/{agentId}/builds/{buildVersion}/instances/{instanceId}")
-    data class Instance(val groupId: String, val agentId: String, val buildVersion: String, val instanceId: String)
-
-    @Location("/api/groups/{groupId}/agents/{agentId}/builds/{buildVersion}/instances/{instanceId}/coverage")
-    data class Coverage(val groupId: String, val agentId: String, val buildVersion: String, val instanceId: String)
-
-    @Location("/api/groups/{groupId}/agents/{agentId}/builds/{buildVersion}/instances/{instanceId}/class-metadata")
-    data class ClassMetadata(val groupId: String, val agentId: String, val buildVersion: String, val instanceId: String)
-
-    @Location("/api/groups/{groupId}/agents/{agentId}/builds/{buildVersion}/instances/{instanceId}/class-metadata/complete")
-    data class ClassMetadataComplete(val groupId: String, val agentId: String, val buildVersion: String, val instanceId: String)
-
-    @Location("/api/groups/{groupId}/agents/{agentId}/builds/{buildVersion}/raw-javascript-coverage")
-    data class RawJavaScriptCoverage(val groupId: String, val agentId: String, val buildVersion: String)
-
-    @Location("/api/groups/{groupId}/tests-metadata")
-    data class TestMetadataRoute(val groupId: String)
+@Location("/api/groups/{groupId}")
+data class Groups(val groupId: String) {
+    @Location("/agents/{agentId}")
+    data class Agents(val parent: Groups, val agentId: String) {
+        @Location("/builds/{buildVersion}")
+        data class BuildVersions(val parent: Agents, val buildVersion: String) {
+            @Location("/instances/{instanceId}")
+            data class Instances(val parent: BuildVersions, val instanceId: String) {
+                @Location("/coverage")
+                data class Coverage(val parent: Instances)
+                @Location("/class-metadata")
+                data class ClassMetadata(val parent: Instances)
+                @Location("/class-metadata/complete")
+                data class ClassMetadataComplete(val parent: Instances)
+            }
+            @Location("/raw-javascript-coverage")
+            data class RawJavaScriptCoverage(val parent: BuildVersions)
+        }
+    }
+    @Location("/tests-metadata")
+    data class TestMetadataRoute(val parent: Groups)
 }
 
 fun Route.putAgentConfig() {
-    put<Paths.Instance> {
+    put<Groups.Agents.BuildVersions.Instances> {
         handleRequest<AgentMetadata> { data ->
             RawDataService.saveAgentConfig(data)
         }
@@ -58,29 +61,29 @@ fun Route.putAgentConfig() {
 }
 
 fun Route.postCoverage() {
-    post<Paths.Coverage> { params ->
+    post<Groups.Agents.BuildVersions.Instances.Coverage> { params ->
         handleRequest<CoverageData> { data ->
-            RawDataService.saveCoverDataPart(params.instanceId, data)
+            RawDataService.saveCoverDataPart(params.parent.instanceId, data)
         }
     }
 }
 
 fun Route.postCLassMetadata() {
-    post<Paths.ClassMetadata> { params ->
+    post<Groups.Agents.BuildVersions.Instances.ClassMetadata> { params ->
         handleRequest<ClassMetadata> { data ->
-            RawDataService.saveInitDataPart(params.instanceId, data)
+            RawDataService.saveInitDataPart(params.parent.instanceId, data)
         }
     }
 }
 
 fun Route.postClassMetadataComplete() {
-    post<Paths.ClassMetadataComplete> { params ->
+    post<Groups.Agents.BuildVersions.Instances.ClassMetadataComplete> { params ->
         call.respond(HttpStatusCode.OK, "Deprecated")
     }
 }
 
 fun Route.postTestMetadata() {
-    post<Paths.TestMetadataRoute> { params ->
+    post<Groups.TestMetadataRoute> { params ->
         handleRequest<AddTestsPayload> { data ->
             RawDataService.saveTestMetadata(data)
         }
@@ -88,9 +91,11 @@ fun Route.postTestMetadata() {
 }
 
 fun Route.postRawJavaScriptCoverage(jsCoverageConverterAddress: String) {
-    post<Paths.RawJavaScriptCoverage> { params ->
+    post<Groups.Agents.BuildVersions.RawJavaScriptCoverage> { params ->
         handleRequest<AddSessionData> { data ->
-            val (groupId, agentId, buildVersion) = params
+            val groupId = params.parent.parent.parent.groupId
+            val agentId = params.parent.parent.agentId
+            val buildVersion = params.parent.buildVersion
             sendPostRequest(
                 "$jsCoverageConverterAddress/groups/${groupId}/agents/${agentId}/builds/${buildVersion}/v8-coverage",
                 data
