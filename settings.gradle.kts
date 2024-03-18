4579798f9c6c3ddc6c9dc5dedb53c69d2fda4b71
iryabov <to.iryabov@gmail.com>
2024-03-18 12:15:28 +0100

    refactor: remove `admin-core` module refactor: remove `test2code-admin` module refactor: move `CRC64` class from `test2code-admin` to `admin-writer` module
    EPMDJ-10811

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,29 +2,22 @@ rootProject.name = "admin"
 
 pluginManagement {
     val kotlinVersion: String by extra
-    val atomicfuVersion: String by extra
     val licenseVersion: String by extra
     val shadowPluginVersion: String by extra
     val grgitVersion: String by extra
-    val jibVersion: String by extra
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
         kotlin("plugin.noarg") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
-        id("kotlinx-atomicfu") version atomicfuVersion
         id("org.ajoberstar.grgit") version grgitVersion
         id("com.github.hierynomus.license") version licenseVersion
         id("com.github.johnrengelman.shadow") version shadowPluginVersion
-        id("com.google.cloud.tools.jib") version jibVersion
     }
     repositories {
         mavenLocal()
         mavenCentral()
         gradlePluginPortal()
-    }
-    resolutionStrategy.eachPlugin {
-        if(requested.id.id == "kotlinx-atomicfu") useModule("org.jetbrains.kotlinx:atomicfu-gradle-plugin:${target.version}")
     }
 }
 
@@ -34,19 +27,10 @@ val includeSharedLib: Settings.(String) -> Unit = {
     project(":$it").projectDir = file(sharedLibsLocalPath).resolve(it)
 }
 
-includeSharedLib("admin-analytics")
 includeSharedLib("common")
-includeSharedLib("dsm")
-includeSharedLib("dsm-annotations")
-includeSharedLib("dsm-test-framework")
-includeSharedLib("ktor-swagger")
 includeSharedLib("plugin-api-admin")
 includeSharedLib("test2code-api")
 includeSharedLib("test2code-common")
 include("admin-auth")
 include("admin-writer")
-include("test2code-admin")
-include("admin-core")
 include("admin-app")
-//include("test-framework")
-include("tests")
