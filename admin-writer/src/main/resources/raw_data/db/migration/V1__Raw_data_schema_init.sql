82b326c95114af1fd6888878de0bde8833758174
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-04-01 20:17:24 +0400

    feat: add sql function to query instances by branch + first instance of base branch

--- admin-writer/src/main/resources/raw_data/db/migration/V1__Raw_data_schema_init.sql
+++ admin-writer/src/main/resources/raw_data/db/migration/V1__Raw_data_schema_init.sql
@@ -34,3 +34,75 @@ CREATE TABLE IF NOT EXISTS raw_data.test_metadata (
     type VARCHAR(255),
     name VARCHAR(2000)
 );
+
+CREATE OR REPLACE FUNCTION get_instance_ids_by_branch(current_hash TEXT, current_branch TEXT, base_branch TEXT, max_depth INT)
+RETURNS TABLE (hash TEXT, parents TEXT, branch TEXT, entry_type TEXT, output_instance_id VARCHAR)
+AS $$
+BEGIN
+    RETURN QUERY
+    WITH RECURSIVE cte AS (
+        SELECT
+            id,
+            instance_id,
+            vcs_metadata ->> 'hash' AS hash,
+            vcs_metadata ->> 'parents' AS parents,
+            vcs_metadata ->> 'branch' AS branch,
+            1 AS depth,
+            0 AS diff_branch,
+            false AS stop
+        FROM
+            raw_data.agent_config
+        WHERE
+            vcs_metadata ->> 'hash' = current_hash
+            AND vcs_metadata ->> 'branch' = ANY(ARRAY[current_branch, base_branch])
+        UNION ALL
+        SELECT
+            t.id,
+            t.instance_id,
+            t.vcs_metadata ->> 'hash' AS hash,
+            t.vcs_metadata ->> 'parents' AS parents,
+            t.vcs_metadata ->> 'branch' AS branch,
+            cte.depth + 1 AS depth,
+            CASE
+                WHEN cte.diff_branch = 1 THEN 1
+                WHEN t.vcs_metadata ->> 'branch' != current_branch THEN 1
+                ELSE 0
+            END AS diff_branch,
+            CASE
+                WHEN cte.diff_branch = 1 OR t.vcs_metadata ->> 'branch' != current_branch THEN true
+                ELSE false
+            END AS stop
+        FROM
+            raw_data.agent_config t
+            JOIN cte ON t.vcs_metadata ->> 'hash' = ANY(string_to_array(cte.parents, ' '))
+        WHERE
+            t.vcs_metadata ->> 'branch' = ANY(ARRAY[current_branch, base_branch])
+            AND cte.depth < max_depth
+            AND NOT cte.stop
+    ),
+    Intermediate AS (
+        SELECT DISTINCT
+            cte.hash,
+            cte.parents,
+            cte.branch,
+            CASE
+                WHEN cte.hash = current_hash AND cte.branch = current_branch THEN 'current'
+                WHEN cte.branch = current_branch THEN 'intermediate'
+                WHEN cte.branch = base_branch THEN 'baseline'
+                ELSE 'error_unknown_instance_encountered'
+            END AS entry_type,
+            cte.instance_id as instance_id
+        FROM
+            cte
+    )
+    SELECT
+        Intermediate.hash,
+        Intermediate.parents,
+        Intermediate.branch,
+        Intermediate.entry_type,
+        Intermediate.instance_id
+    FROM
+        Intermediate;
+END;
+$$ LANGUAGE plpgsql;
+
