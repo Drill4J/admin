b646233a5e6a6f5df767f0356bffa2bf9fece918
iryabov <to.iryabov@gmail.com>
2025-03-03 14:21:11 +0100

    fix: return deprecated view_methods_coverage and matview_methods_coverage to let tests passed
    EPMDJ-10994

--- admin-writer/src/main/resources/raw_data/db/migration/R__2_Mat_Views.sql
+++ admin-writer/src/main/resources/raw_data/db/migration/R__2_Mat_Views.sql
@@ -1,9 +1,34 @@
 -----------------------------------------------------------------
 -- Delete all materialized views
 -----------------------------------------------------------------
+DROP MATERIALIZED VIEW IF EXISTS raw_data.matview_methods_coverage;
 DROP MATERIALIZED VIEW IF EXISTS raw_data.matview_methods_coverage_v2;
 DROP MATERIALIZED VIEW IF EXISTS raw_data.matview_test_sessions;
 DROP MATERIALIZED VIEW IF EXISTS raw_data.matview_recommended_tests;
+
+-----------------------------------------------------------------
+--Deprecated, use matview_methods_coverage_v2
+-----------------------------------------------------------------
+CREATE MATERIALIZED VIEW IF NOT EXISTS raw_data.matview_methods_coverage AS
+    SELECT
+        MIN(group_id) AS group_id,
+        MIN(app_id) AS app_id,
+        signature,
+        body_checksum,
+        probes_count,
+        build_id,
+        BIT_OR(probes) AS probes,
+        BIT_COUNT(BIT_OR(probes)) AS covered_probes,
+        MIN(created_at) AS created_at,
+        MIN(branch) AS branch,
+        env_id,
+		test_definition_id,
+        test_result,
+        test_task_id
+    FROM raw_data.view_methods_coverage
+	GROUP BY signature, body_checksum, probes_count, build_id, env_id, test_definition_id, test_result, test_task_id;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_matview_methods_coverage_pk ON raw_data.matview_methods_coverage (signature, body_checksum, probes_count, build_id, env_id, test_definition_id, test_result, test_task_id);
 
 -----------------------------------------------------------------
 
