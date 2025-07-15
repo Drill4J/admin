6c9af5e83105a55b02976b87d101380a538c9285
iryabov <to.iryabov@gmail.com>
2025-07-15 13:57:47 +0200

    feat: change calculation session coverage algorithm using direct reference `test_session_id` from `raw_data.coverage`
    EPMDJ-11024

--- admin-writer/src/main/resources/raw_data/db/migration/V19__Coverage_test_session_id.sql
+++ admin-writer/src/main/resources/raw_data/db/migration/V19__Coverage_test_session_id.sql
@@ -1,2 +1,8 @@
 ALTER TABLE raw_data.coverage
 ADD COLUMN test_session_id VARCHAR;
+
+UPDATE raw_data.coverage SET test_session_id = (
+    SELECT tl.test_session_id
+    FROM raw_data.test_launches tl
+    WHERE tl.id = raw_data.coverage.test_id
+) WHERE test_session_id IS NULL AND test_id IS NOT NULL;
