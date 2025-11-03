e4baf8f6cb5fcc5c000c7683a8404b8977ce5f33
iryabov <to.iryabov@gmail.com>
2025-11-03 17:05:40 +0100

    feat: update method_id column to enforce length constraint in metrics tables
    EPMDJ-11156

--- admin-metrics/src/main/resources/metrics/db/migration/V4__Metrics_tables.sql
+++ admin-metrics/src/main/resources/metrics/db/migration/V4__Metrics_tables.sql
@@ -19,7 +19,7 @@ CREATE INDEX ON metrics.builds_table(group_id, app_id, build_id, created_at);
 CREATE TABLE IF NOT EXISTS metrics.methods_table (
     group_id VARCHAR,
     app_id VARCHAR,
-    method_id VARCHAR,
+    method_id VARCHAR(32),
     signature VARCHAR,
     method_name VARCHAR,
     class_name VARCHAR,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS metrics.build_methods_table (
     group_id VARCHAR,
     app_id VARCHAR,
     build_id VARCHAR,
-    method_id VARCHAR,
+    method_id VARCHAR(32),
     probes_start INT,
     method_num INT,
     PRIMARY KEY (group_id, app_id, build_id, method_id)
@@ -88,7 +88,7 @@ CREATE INDEX ON metrics.test_sessions_table(group_id, test_session_id, created_a
 CREATE TABLE IF NOT EXISTS metrics.method_coverage_table (
     group_id VARCHAR,
     app_id VARCHAR,
-    method_id VARCHAR,
+    method_id VARCHAR(32),
     build_id VARCHAR,
     app_env_id VARCHAR,
     test_session_id VARCHAR,
@@ -112,7 +112,7 @@ CREATE INDEX ON metrics.method_coverage_table(group_id, app_id, method_id, test_
 CREATE TABLE IF NOT EXISTS metrics.method_smartcoverage_table (
     group_id VARCHAR,
     app_id VARCHAR,
-    method_id VARCHAR,
+    method_id VARCHAR(32),
     app_env_id VARCHAR,
     branch VARCHAR,
     test_tags VARCHAR[],
