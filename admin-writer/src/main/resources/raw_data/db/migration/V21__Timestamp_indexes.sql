f74b403b0943298c6b221c2a134b2af674e4a23d
iryabov <to.iryabov@gmail.com>
2025-11-05 09:23:14 +0100

    feat: add indexes to improve query performance on various raw_data tables
    EPMDJ-11156

---
+++ admin-writer/src/main/resources/raw_data/db/migration/V21__Timestamp_indexes.sql
@@ -0,0 +1,11 @@
+CREATE INDEX IF NOT EXISTS idx_builds_created_at ON raw_data.builds(created_at);
+CREATE INDEX IF NOT EXISTS idx_methods_created_at ON raw_data.methods(created_at);
+CREATE INDEX IF NOT EXISTS idx_instances_created_at ON raw_data.instances(created_at);
+CREATE INDEX IF NOT EXISTS idx_coverage_created_at ON raw_data.coverage(created_at);
+CREATE INDEX IF NOT EXISTS idx_test_launches_created_at ON raw_data.test_launches(created_at);
+CREATE INDEX IF NOT EXISTS idx_test_sessions_created_at ON raw_data.test_sessions(created_at);
+CREATE INDEX IF NOT EXISTS idx_test_session_builds_created_at ON raw_data.test_session_builds(created_at);
+CREATE INDEX IF NOT EXISTS idx_test_definitions_created_at ON raw_data.test_definitions(created_at);
+
+CREATE INDEX IF NOT EXISTS idx_methods_group_id_app_id_signature ON raw_data.methods(group_id, app_id, signature);
+CREATE INDEX IF NOT EXISTS idx_methods_group_id_app_id_build_id ON raw_data.methods(group_id, app_id, build_id);
