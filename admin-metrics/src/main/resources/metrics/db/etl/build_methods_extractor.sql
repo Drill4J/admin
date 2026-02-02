6fcaf782baea4a7487b6df4f8aa3977b27668cfd
iryabov <to.iryabov@gmail.com>
2026-02-02 11:13:40 +0100

    feat: remove method ignore rules from build and coverage extractors for cleaner queries

--- admin-metrics/src/main/resources/metrics/db/etl/build_methods_extractor.sql
+++ admin-metrics/src/main/resources/metrics/db/etl/build_methods_extractor.sql
@@ -10,7 +10,7 @@ SELECT
     m.class_name,
     m.method_params,
     m.return_type,
-    m.created_at,
+    bm.created_at,
     DATE_TRUNC('day', m.created_at) AS created_at_day
 FROM raw_data.build_methods bm
 JOIN raw_data.methods m ON m.method_id = bm.method_id AND m.app_id = bm.app_id AND m.group_id = bm.group_id
@@ -18,13 +18,5 @@ WHERE bm.group_id = :group_id
     AND bm.created_at > :since_timestamp
     AND bm.created_at <= :until_timestamp
     AND m.probes_count > 0
-    AND NOT EXISTS (SELECT 1
-        FROM raw_data.method_ignore_rules r
-        WHERE r.group_id = m.group_id
-            AND r.app_id = m.app_id
-            AND (r.name_pattern IS NOT NULL AND m.method_name::text ~ r.name_pattern::text
-                OR r.classname_pattern IS NOT NULL AND m.class_name::text ~ r.classname_pattern::text
-                OR r.annotations_pattern IS NOT NULL AND bm.annotations::text ~ r.annotations_pattern::text
-                OR r.class_annotations_pattern IS NOT NULL AND bm.class_annotations::text ~ r.class_annotations_pattern::text))
 ORDER BY bm.created_at ASC, bm.group_id, bm.method_id
 LIMIT :limit
