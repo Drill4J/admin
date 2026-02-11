677322c2d41cf0603e38c66ad5c2c07f34a90a50
iryabov <to.iryabov@gmail.com>
2026-02-11 11:39:44 +0100

    fix: increase commitMessage field length to LONG_TEXT_LENGTH
    (cherry picked from commit b19de1064c1b504f5581cc814543c20e562f4fe0)

--- admin-writer/src/main/kotlin/com/epam/drill/admin/writer/rawdata/table/BuildTable.kt
+++ admin-writer/src/main/kotlin/com/epam/drill/admin/writer/rawdata/table/BuildTable.kt
@@ -26,7 +26,7 @@ object BuildTable : StringIdTable("raw_data.builds") {
     val instanceId = varchar("instance_id", SHORT_TEXT_LENGTH).nullable()
     val branch = varchar("branch", SHORT_TEXT_LENGTH).nullable()
     val commitAuthor = varchar("commit_author", SHORT_TEXT_LENGTH).nullable()
-    val commitMessage = varchar("commit_message", SHORT_TEXT_LENGTH).nullable()
+    val commitMessage = varchar("commit_message", LONG_TEXT_LENGTH).nullable()
     val committedAt = datetime("committed_at").nullable()
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
     val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
