72153b831f8e724e98596fe94c559e1e81a3f4e9
iryabov <Igor_Riabov@epam.com>
2026-05-20 08:49:13 +0200

    refactor: simplify EtlMeter by removing internal metric caches

--- admin-etl/src/main/kotlin/com/epam/drill/admin/etl/config/EtlMeter.kt
+++ admin-etl/src/main/kotlin/com/epam/drill/admin/etl/config/EtlMeter.kt
@@ -19,44 +19,30 @@ import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
 
 class EtlMeter(val registry: MeterRegistry) {
-    private val longGauges = ConcurrentHashMap<String, AtomicLong>()
-    private val counters = ConcurrentHashMap<String, Counter>()
-    private val timers = ConcurrentHashMap<String, Timer>()
-
     fun registerLongGauge(metricName: String, jobName: String, groupId: String): AtomicLong {
-        val key = "$metricName:$jobName:$groupId"
-        return longGauges.compute(key) { _, oldValue ->
-            oldValue?.also { it.set(0) } ?: AtomicLong(0).also { newValue ->
-                Gauge.builder(metricName) {
-                    newValue.get().toDouble()
-                }.tag("jobName", jobName)
-                    .tag("groupId", groupId)
-                    .register(registry)
-            }
-        } ?: throw IllegalStateException("Failed to create gauge for $key")
+        val value = AtomicLong(0)
+        Gauge.builder(metricName) { value.get() }
+            .tag("jobName", jobName)
+            .tag("groupId", groupId)
+            .register(registry)
+        return value
     }
 
     fun registerCounter(metricName: String, jobName: String, groupId: String): Counter {
-        val key = "$metricName:$jobName:$groupId"
-        return counters.computeIfAbsent(key) { key ->
-            Counter.builder(metricName)
-                .tag("jobName", jobName)
-                .tag("groupId", groupId)
-                .register(registry)
-        }
+        return Counter.builder(metricName)
+            .tag("jobName", jobName)
+            .tag("groupId", groupId)
+            .register(registry)
     }
 
+
     fun registerTimer(metricName: String, jobName: String, groupId: String): Timer {
-        val key = "$metricName:$jobName:$groupId"
-        return timers.computeIfAbsent(key) {
-            Timer.builder(metricName)
-                .tag("jobName", jobName)
-                .tag("groupId", groupId)
-                .register(registry)
-        }
+        return Timer.builder(metricName)
+            .tag("jobName", jobName)
+            .tag("groupId", groupId)
+            .register(registry)
     }
 }
