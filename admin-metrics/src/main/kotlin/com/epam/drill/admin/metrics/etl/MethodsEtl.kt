12893498a7b686298af80409443530482c47b9ef
iryabov <Igor_Riabov@epam.com>
2026-08-18 11:59:57 +0200

    feat: add methods extractor and update build methods extractor SQL query

--- admin-metrics/src/main/kotlin/com/epam/drill/admin/metrics/etl/MethodsEtl.kt
+++ admin-metrics/src/main/kotlin/com/epam/drill/admin/metrics/etl/MethodsEtl.kt
@@ -35,6 +35,17 @@ val EtlConfig.buildMethodsExtractor
         metrics = metrics,
     )
 
+val EtlConfig.methodsExtractor
+    get() = UntypedSqlDataExtractor(
+        name = "methods",
+        sqlQuery = fromResource("/metrics/db/etl/methods_extractor.sql"),
+        database = RawDataWriterDatabaseConfig.database,
+        fetchSize = fetchSize,
+        extractionLimit = extractionLimit,
+        lastExtractedAtColumnName = "created_at",
+        metrics = metrics,
+    )
+
 val EtlConfig.buildMethodsLoader
     get() = UntypedSqlDataLoader(
         name = "build_methods",
@@ -62,7 +73,7 @@ val EtlConfig.buildMethodsPipeline
 
 val EtlConfig.methodsPipeline
     get() = pipeline("methods")
-        .extractWith(buildMethodsExtractor)
+        .extractWith(methodsExtractor)
         .aggregateBy("group_id", "app_id", "method_id") { _, next ->
             UntypedRow(next.timestamp, next)
         }
